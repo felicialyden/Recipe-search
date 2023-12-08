d9735dc94fa95e62910f9fabe1023d8bc9d27c7a
Felicia <felicia.lyden@gmail.com>
2023-12-08 08:27:04 +0100

    chore: set up Prisma and connect to database

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-const port = 3001
 
 app.use(express.json());
 app.use(cors())
@@ -11,8 +10,5 @@ app.get('/', (_req, res) => {
   res.send('Hello World!')
 })
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export default app;
 
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,5 @@
-console.log('hello')
+import app from "./app";
+
+const port = 3001;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
