6c7a24fcc1b4f9960fe675060773fea5546a2e31
Felicia Lydén <felicia.lyden@gmail.com>
2024-01-17 18:52:48 +0100

    include cuisine and diet in get call

--- src/api/recipes.ts
+++ src/api/recipes.ts
@@ -9,9 +9,13 @@ const apiKey = process.env.API_KEY;
 
 router.get("/", async (req, res, next) => {
   const ingredients = req.query.ingredients
+  const cuisine = req.query.cuisine
+  const diet = req.query.diet
+  console.log(cuisine, diet)
+
     try {
       const response = await fetch(
-        `${url}/complexSearch?includeIngredients=${ingredients}&ignorePantry=true&sort=max-used-ingredients&sortDirection=desc&apiKey=${apiKey}`
+        `${url}/complexSearch?includeIngredients=${ingredients}&diet=${diet}&&cuisine=${cuisine}&ignorePantry=true&sort=max-used-ingredients&sortDirection=desc&apiKey=${apiKey}`
       );
       const json = await response.json()
       res.send(json);
